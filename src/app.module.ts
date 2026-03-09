329a6e88dd3c495c6907c0932387887004cb6a05
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-03-09 05:24:25 -0300

    refactor: correcting archive structure; adding prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { PrismaService } from './modules/prisma/prisma.service';
 
 @Module({
-  imports: [ UserModule],
+  imports: [UserModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
