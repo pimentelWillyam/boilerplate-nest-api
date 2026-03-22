1a2634e500cf1dd7d43804dcb961f42b4c06f420
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-03-22 07:36:33 -0300

    refactor: adding restraints to update user dto

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,12 +1,16 @@
-import { IsEmail, IsString } from 'class-validator'
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator'
 
 export class UpdateUserDto {
+  @IsOptional()
+  @IsEmail()
+  email?: string
+
+  @IsOptional()
   @IsString()
   login?: string
 
+  @IsOptional()
   @IsString()
+  @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres' })
   password?: string
-
-  @IsEmail()
-  email?: string
 }
