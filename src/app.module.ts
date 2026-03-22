65d9064809dfe23a3c3a6fef96d10c6c738f5ce4
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-03-22 07:08:18 -0300

    refactor: using configmodule on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './modules/user/user.module'
 import { PrismaModule } from './modules/prisma/prisma.module'
 import { PrismaService } from './modules/prisma/prisma.service'
+import { PrismaModule } from './infra/database/prisma.module'
+import { getConfigModuleOptions } from './config/config-module.option'
 
 @Module({
-  imports: [UserModule, PrismaModule],
+  imports: [ConfigModule.forRoot(getConfigModuleOptions()), UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
