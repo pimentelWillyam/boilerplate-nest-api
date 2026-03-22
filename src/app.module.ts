36079247545613b3d1487943ff3525adaa0cdaa9
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-03-22 07:08:35 -0300

    refactor: removing duplicate of prismaservices

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,12 @@ import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './modules/user/user.module'
-import { PrismaModule } from './modules/prisma/prisma.module'
-import { PrismaService } from './modules/prisma/prisma.service'
 import { PrismaModule } from './infra/database/prisma.module'
 import { getConfigModuleOptions } from './config/config-module.option'
 
 @Module({
   imports: [ConfigModule.forRoot(getConfigModuleOptions()), UserModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
   providers: [AppService],
 })
 export class AppModule {}
