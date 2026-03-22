a6b8a8924d63fce315f28a575b85871bc220d02f
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-03-22 07:36:13 -0300

    refactor: adding minlenght to create-user dto

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString } from 'class-validator'
+import { IsEmail, IsString, MinLength } from 'class-validator'
 
 export class CreateUserDto {
   @IsEmail()
@@ -8,5 +8,6 @@ export class CreateUserDto {
   login!: string
 
   @IsString()
+  @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres' })
   password!: string
 }
