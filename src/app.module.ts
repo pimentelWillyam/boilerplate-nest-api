1d902042cb7e5dc3b47642f4cacdc3bf9bda880c
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-04-01 20:51:32 -0300

    refactor: changing prisma module to generic database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './modules/user/user.module'
-import { PrismaModule } from './infra/database/prisma.module'
 import { AppConfig } from './config/app-config'
+import { DatabaseModule } from './infra/database/database.module'
 
 @Module({
-  imports: [ConfigModule.forRoot(AppConfig.getConfigModuleOptions()), UserModule, PrismaModule],
+  imports: [ConfigModule.forRoot(AppConfig.getConfigModuleOptions()), DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
