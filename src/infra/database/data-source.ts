e7fddc77ec0f389d6df44b0e2ed01fe95e4d4add
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-04-18 12:37:27 -0300

    fix: making datasource wowrk with entiti files and setting dev as default env file

--- src/infra/database/data-source.ts
+++ src/infra/database/data-source.ts
@@ -1,15 +1,17 @@
 import { DataSource } from 'typeorm';
-import { User } from '../../modules/user/user.entity';
 import * as dotenv from 'dotenv';
+import * as path from 'path';
 
-// Load environment variables
-dotenv.config();
+const envFile = `.env.${process.env.NODE_ENV || 'dev'}`;
+
+dotenv.config({
+  path: path.resolve(process.cwd(), envFile),
+});
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: [User],
+  entities: ['src/**/*.entity.ts'],
   migrations: ['src/infra/database/migrations/*.ts'],
   synchronize: false,
-  logging: false,
 });
